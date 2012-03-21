ee974ab9caefe25c4db0763ba1e1182d028668ca
ranveer5289 <ranveer.raghu@gmail.com>
2012-03-21 07:35:36 +0530

    Now Show notification after application restart.

--- Application/MainSpotify.cs
+++ Application/MainSpotify.cs
@@ -18,19 +18,21 @@ class NameChangeTracker
 {
         // Fields
         private const uint EVENT_OBJECT_NAMECHANGE = 0x800c;
+        private const uint EVENT_OBJECT_CREATE = 0x00008000;
         private const uint WINEVENT_OUTOFCONTEXT = 0;
 
         public static string track = null;
         public static string artist = null;
         public static Notify notify = null;
         public static TrackMetadata TMD = null;
-
+        public static ProcessInformation PSI = null;
+        public static IntPtr hwnd_spotify;
+        public static int processid;
 
         //Dll Imports
 
         [DllImport("user32")]
                 internal static extern  bool GetMessage(ref Message lpMsg, IntPtr handle, uint mMsgFilterInMain, uint mMsgFilterMax);
-
 
         [DllImport("user32.dll")]
                 private static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, 
@@ -43,14 +45,23 @@ class NameChangeTracker
 
         public static void nameChanger()
         {
-                TMD = new TrackMetadata();
+                hwnd_spotify = PSI.getSpotify();
+                processid = PSI.getProcessId(hwnd_spotify);
                 track = TMD.getTrack();
                 artist  = TMD.getArtist();
+        }
+
+        public static void create_object()
+        {
+                PSI = new ProcessInformation();
+                TMD = new TrackMetadata();
                 notify = new Notify();
+
         }
 
 
         private static WinEventDelegate procDelegate = new WinEventDelegate(NameChangeTracker.WinEventProc);
+        private static WinEventDelegate procDelegate_start = new WinEventDelegate(NameChangeTracker.WinEventProc_start);
 
         private delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int 
                         idChild, uint dwEventThread, uint dwmsEventTime);
@@ -73,29 +84,57 @@ class NameChangeTracker
                 }
         }
 
+        private static void WinEventProc_start(IntPtr hWinEventHook_start, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+
+        {
+                if(idObject==0 && idChild==0)
+                {
+                        nameChanger();
+                        if(hwnd.ToInt32() == hwnd_spotify.ToInt32() )
+                        {
+                                Console.WriteLine("checking hwnd");
+                                if(eventType == EVENT_OBJECT_CREATE)
+                                {              
+                                        Console.WriteLine("create event");
+                                        create_object();
+                                        IntPtr hWinEventHook = SetWinEventHook(0x0800c, 0x800c, IntPtr.Zero, procDelegate, Convert.ToUInt32(processid), 0, 0);
+                                }
+                        }
+                }
+        }
+
 
         public static void Main()
         {
                 ProcessInformation PSI = new ProcessInformation();
 
 
-                if(PSI.isAvailable())
-                {         
-                        IntPtr hWnd = PSI.getSpotify();
-                        int processid = PSI.getProcessId(hWnd);
 
-                        IntPtr hWinEventHook = SetWinEventHook(0x800c, 0x800c, IntPtr.Zero, procDelegate, Convert.ToUInt32(processid), 0, 0);
+                if(PSI.isAvailable())
+                { 
+
+                        NameChangeTracker.create_object();
+                        NameChangeTracker.nameChanger();
+                        IntPtr hWnd = NameChangeTracker.hwnd_spotify;
+                        int pid = NameChangeTracker.processid;
+
+                        IntPtr hWinEventHook = SetWinEventHook(0x0800c, 0x800c, IntPtr.Zero, procDelegate, Convert.ToUInt32(pid), 0, 0);
+                        IntPtr hWinEventHook_start = SetWinEventHook(0x00008000,0x00008000,IntPtr.Zero, procDelegate_start, 0, 0, 0);
                         //MessageBox.Show("Tracking name changes on HWNDs, close message box to exit.");
 
                         Message msg = new Message();
 
                         while(GetMessage(ref msg,hWnd,0,0))
+                        {}
 
 
-                                UnhookWinEvent(hWinEventHook);
+                        // UnhookWinEvent(hWinEventHook);
+                        // UnhookWinEvent(hWinEventHook_start);
+
                 }
 
                 else
                         MessageBox.Show("check spotify running or not");
         }
 }
+
