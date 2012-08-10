b64edf69fb98f35caa9d4dd9de141efdfa279f81
ranveer5289 <ranveer.raghu@gmail.com>
2012-08-10 16:39:57 +0530

    Compile project

---
+++ SpotifyNotifierSnarl/compile_NET3.5.bat
@@ -0,0 +1,4 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /win32icon:SpotifySnarl.ico /out:SpotifyNotifierSnarl.exe *.cs
+echo "Compilation successful now run SpotifyNotifierGrowl.exe"
+
