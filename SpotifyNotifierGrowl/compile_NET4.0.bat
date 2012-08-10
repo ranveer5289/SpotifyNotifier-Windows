4ef09dd9a85b62003041311fe2d5fac6d69de945
ranveer5289 <ranveer.raghu@gmail.com>
2012-08-10 16:36:43 +0530

    Compiled on .Net4.0

--- SpotifyNotifierGrowl/compile_NET4.0.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /r:Growl.Connector.dll,Growl.CoreLibrary.dll /out:SpotifyNotifierGrowl.exe *.cs
-echo "Compilation successful now run SpotifyNotifierGrowl.exe"
